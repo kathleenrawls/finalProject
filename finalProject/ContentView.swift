ba6ee88bb58fab88e9b1329c6055cfaf943f469e
Kathleen Rawls <kathleen.rawls@icloud.com>
2024-07-26 13:15:57 -0400

    First commit

--- finalProject/ContentView.swift
+++ finalProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Group 5 final project")
         }
         .padding()
     }
