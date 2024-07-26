13268232d44303489db7ce8269f4da4ec292acdb
Kathleen Rawls <kathleen.rawls@icloud.com>
2024-07-26 13:14:04 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
