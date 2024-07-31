0af6265b6cefeaae8b92149c91e1b7065dad5e26
Kathleen Rawls <kathleen.rawls@icloud.com>
2024-07-31 11:38:54 -0400

    Added articleView file

---
+++ finalProject/articleView.swift
@@ -0,0 +1,18 @@
+//
+//  articleView.swift
+//  finalProject
+//
+//  Created by scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct articleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    articleView()
+}
