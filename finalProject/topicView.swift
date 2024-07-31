9a266e081602433d0a884d320f505410b4d2d09c
Kathleen Rawls <kathleen.rawls@icloud.com>
2024-07-31 10:44:29 -0400

    Adding plastics article information

--- finalProject/topicView.swift
+++ finalProject/topicView.swift
@@ -14,14 +14,17 @@ struct topicView: View {
     @State public var articles = [
         ["title": "Water article title",
          "topic": "water",
+         "imgSrc": "",
          "author": "Sustainable Home",
          "content": "text"],
-        ["title": "Plastic article title",
+        ["title": "5 Craft Ideas to Reuse Plastic Bottles",
          "topic": "plastic",
-         "author": "Sustainable Home",
-         "content": "text1"],
+         "imgSrc": "plasticBottles",
+         "author": "Kathleen from Sustainable Home",
+         "content": "\tIn the world, almost 60 million plastic bottles are thrown away each day. These plastics are not good for the environment, and they will stay in landfills and water sources for a long time. Plastic bottles take up to 1000 years to completely decompose, which means that they will be in the water for a long time.\n\n\tHowever, there are ways that we can reduce the number of plastic bottles that end up this way. One way is through reusing them. Below are five fun crafts you can do with plastic bottles to keep them from being thrown away.\n\n\t1) Pen holder: Cut the top part off of a plastic bottle and use the bottom to make a creative pen holder. You can paint it or decorate it however you want.\n\t2) Vase: You can paint or decorate an entire bottle to transform it into a lovely flower vase.\n\t3) Sprinkler system: If you cut many holes into the sides of a plastic bottle, you can connect it to a hose to use as an outdoor sprinkler system. The water will spray out through all of the holes.\n\t4) Creative lamp fixtures: If you cut off the top part of a bottle, you can put it around a small light bulb to create a light fixture. You can make a standing lamp or a hanging bulb decoration.\n\t5) Mini worlds: If you are interested in dioramas or miniature creations, why not make an entire miniature world inside a bottle?"],
         ["title": "Deforestation article title",
          "topic": "deforestation",
+         "imgSrc": "",
          "author": "Sustainable Home",
          "content": "text3"]]
     
